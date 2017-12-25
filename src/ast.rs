b0b784882ca0f2d3f86d2f15454f6680caf3f74c
Hans-Martin Will <hwill@acm.org>
2017-12-24 17:19:59 -0800

    Expressions with AST construction compile

--- src/ast.rs
+++ src/ast.rs
@@ -4,5 +4,74 @@ pub enum SqlStatement {
 }
 
 pub enum Statement {
-    SelectStatement,
+    SelectStatement(SelectStatement),
 }
+
+pub struct SelectStatement {
+
+}
+
+pub enum UnaryOperator {
+    Negate,
+    Not,
+    IsNull,
+}
+
+pub enum BinaryOperator {
+    Multiply,
+    Divide,
+    Add,
+    Subtract,
+    Concat,
+    And,
+    Or,
+}
+
+pub enum ComparisonOperator {
+    Equal,
+    NotEqual,
+    LessThan,
+    LessEqual,
+    GreaterThan,
+    GreaterEqual,
+    Like,
+    Is,
+    IsNot,
+}
+
+pub enum Expression {
+    Literal(Literal),
+    QualifiedIdentifier(Vec<String>),
+    TupleExpression(Vec<Box<Expression>>),
+    SelectStatement(SelectStatement),
+
+    UnaryExpression(UnaryOperator, Box<Expression>),
+    BinaryExpression(BinaryOperator, Box<Expression>, Box<Expression>),
+    CompareExpression(ComparisonOperator, Box<Expression>, Box<Expression>),
+
+    InExpression(Box<Expression>, SetSpecification),
+    BetweenExpression(Box<Expression>, Box<Expression>, Box<Expression>),
+
+    CaseExpression(Box<Expression>, Vec<WhenClause>, Option<Box<Expression>>),
+}
+
+pub enum SetSpecification {
+    Select(SelectStatement),
+    List(Vec<Box<Expression>>),
+    Name(Vec<String>),
+}
+
+pub struct WhenClause {
+    pub guard: Box<Expression>,
+    pub body: Box<Expression>,
+}
+
+pub enum Literal {
+    StringLiteral(String),
+    NumericLiteral(String),
+    Null,
+    CurrentTime,
+    CurrentDate,
+    CurrentTimestamp,
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
 pub mod ast;
-pub mod tokenizer;
-pub mod parser;
+pub mod sql;
 
